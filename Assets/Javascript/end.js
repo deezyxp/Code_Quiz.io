fd432623c18c95991e89f67b81f67dcd36677cb1
deezy <danielshiaolin@gmail.com>
2020-09-01 19:25:40 +1000

    debugging

--- Assets/Javascript/end.js
+++ Assets/Javascript/end.js
@@ -27,6 +27,6 @@ saveScore = e => {
     highScores.sort((a,b) => b.score - a.score)
     highScores.splice(5);
     localStorage.setItem("highScores", JSON.stringify(highScores));
-    window.location.assign("/")
+    window.location.assign("index.html")
 };
 
