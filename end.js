7364eac031a9971e81baa71bfda6cd0a271e6bf0
deezy <danielshiaolin@gmail.com>
2020-09-01 11:30:39 +1000

    added in save button

--- end.js
+++ end.js
@@ -1,4 +1,13 @@
-var username = document.querySelector('#username')
-var saveBtnEl = document.querySelector('#saveBtn')
+const username = document.querySelector('#username')
+const saveBtn = document.querySelector('#saveBtn')
 var finalScoreEl = document.querySelector('#finalScore')
 var mostRecentScoreEl = document.querySelector('#mostRecentScore')
+
+username.addEventListener("keyup", () => {
+    saveBtn.disabled = !username.value;
+});
+
+saveScore = e => {
+    console.log("clicked button");
+    e.preventDefault();
+};
